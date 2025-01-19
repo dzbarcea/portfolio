8bc30de1ff8de15f4b58fdab445266f513368969
dzbarcea <david.edward.zbarcea@gmail.com>
2025-01-19 12:29:40 -0500

    added contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,44 @@
+import {Icon} from '@iconify/react';
+import HoverCard from '@/components/HoverCard/HoverCard';
+
 const Page = () => {
     return (
         <>
+            <div className='flex flex-col gap-2 items-start'>
+                <h3>Email</h3>
+
+                <HoverCard>
+                    <a className='flex items-center gap-1 p-2' href='mailto:david.e.zbarcea@gmail.com'>
+                        david.e.zbarcea@gmail.com
+                        <Icon icon='ci:external-link'/>
+                    </a>
+                </HoverCard>
+            </div>
+
+            <div className='flex flex-col gap-2 items-start'>
+                <h3>Social Media</h3>
+
+                <div className='flex flex-wrap gap-2 items-start'>
+                    <HoverCard>
+                        <a className='flex items-center gap-1 p-2' href='https://www.linkedin.com/in/david-z-b495b9140/' target='_blank'>
+                            LinkedIn
+                            <Icon icon='ci:external-link'/>
+                        </a>
+                    </HoverCard>
+                    <HoverCard>
+                        <a className='flex items-center gap-1 p-2' href='https://github.com/dzbarcea' target='_blank'>
+                            GitHub
+                            <Icon icon='ci:external-link'/>
+                        </a>
+                    </HoverCard>
+                    <HoverCard>
+                        <a className='flex items-center gap-1 p-2' href='https://www.instagram.com/dzbarcea/' target='_blank'>
+                            Instagram
+                            <Icon icon='ci:external-link'/>
+                        </a>
+                    </HoverCard>
+                </div>
+            </div>
         </>
     );
 }
--- src/components/HoverCard/HoverCard.tsx
+++ src/components/HoverCard/HoverCard.tsx
@@ -2,18 +2,13 @@ import { ReactNode } from 'react';
 
 interface HoverCardProps {
     children?: ReactNode;
-    href?: string;
 }
 
-const HoverCard = ({children, href}: HoverCardProps) => {
-    const contents = (
+const HoverCard = ({ children }: HoverCardProps) => {
+    return (
         <div className='hover-container rounded-lg'>
             {children}
         </div>
-    );
-
-    return (
-        href ? <a href={href}>{contents}</a> : contents
     );
 }
 
