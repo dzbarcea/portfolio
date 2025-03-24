b5f490643ada96ea8d7b528b7d28784be10f3515
dzbarcea <david.edward.zbarcea@gmail.com>
2025-03-24 11:32:43 -0400

    escaped apostrophe

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const Page = () => {
     return (
         <div className='flex items-start flex-col gap-2 pt-4 text-xl lg:w-1/2'>
-            <p>Hey, I’m David. I build clean, user-friendly websites that blend solid code with smart design.</p>
+            <p>Hey, I&apos;m David. I build clean, user-friendly websites that blend solid code with smart design.</p>
             <p>With 3 years of experience in development and UI/UX, I love bringing creative ideas to life through thoughtful problem-solving.</p>
             <p>Take a look at some of my work, or contact me via email or socials.</p>
 
