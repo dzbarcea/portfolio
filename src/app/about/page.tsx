c1de943ab2559ce97b268a0cfaf9cddf14e331ae
dzbarcea <david.edward.zbarcea@gmail.com>
2025-03-24 08:50:28 -0400

    changed about text and added a CTA button

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,17 @@
 // import ImageController from '@/components/ImageController/ImageController';
 
+import Link from 'next/link';
+
 const Page = () => {
     return (
-        <div className='flex flex-col gap-2 pt-4 text-xl lg:w-1/2'>
-            <p>Hi, I’m David. I’m passionate about writing clean, efficient code and designing user experiences that
-                satisfy and engage.</p>
-            <p>With 2 years of experience in Fullstack Development and UI/UX Design, I thrive in fast-paced,
-                high-pressure environments where creativity and problem-solving come together.</p>
-            <p>I learn lessons from everything I do. They shape who I am and how I approach my work. Check out some
-                of my favorite interests below:</p>
+        <div className='flex items-start flex-col gap-2 pt-4 text-xl lg:w-1/2'>
+            <p>Hi, I’m David. I’m passionate about writing clean, efficient code and developing websites that
+                satisfy and engage users.</p>
+            <p>With 3 years of experience in Fullstack Development and UI/UX Design, I thrive in
+                environments where creativity and problem-solving come together. Click below to see
+                some of the projects I've worked on.</p>
 
-            {/*<ImageController/>*/}
+            <Link href='/work' className='border-container clickable py-2 px-4 rounded-full'>View My Work</Link>
         </div>
     );
 }
